97d61ab2e4f4186aeb36eb6e22c761b9a19dcb07
Pham Minh Hy <hypm@MECORP.LOCAL>
2022-08-02 16:53:16 +0700

    init

---
+++ tests/2_functional-tests.js
@@ -0,0 +1,112 @@
+const chai = require('chai');
+const assert = chai.assert;
+
+const server = require('../server');
+
+const chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+
+suite('Functional Tests', function () {
+  this.timeout(5000);
+  suite('Integration tests with chai-http', function () {
+    // #1
+    test('Test GET /hello with no name', function (done) {
+      chai
+        .request(server)
+        .get('/hello')
+        .end(function (err, res) {
+          assert.equal(res.status, 200, 'Response status should be 200');
+          assert.equal(res.text, 'hello Guest', 'Response should be "hello Guest"');
+          done();
+        });
+    });
+    // #2
+    test('Test GET /hello with your name', function (done) {
+      chai
+        .request(server)
+        .get('/hello?name=PhamMinhHy')
+        .end(function (err, res) {
+          assert.equal(res.status, 200, 'Response status should be 200');
+          assert.equal(res.text, 'hello PhamMinhHy', 'Response should be "hello PhamMinhHy"');
+          done();
+        });
+    });
+    // #3
+    test('Send {surname: "Colombo"}', function (done) {
+      chai
+        .request(server)
+        .put('/travellers')
+        .send({
+          "surname": "Colombo"
+        })
+        .end(function (err, res) {
+          assert.equal(res.status, 200, 'Response status should be 200');
+          assert.equal(res.type, 'application/json', 'Response type should be "application/json"');
+          assert.equal(res.body.name, 'Cristoforo', 'Response body.name should be "Cristoforo"');
+          assert.equal(res.body.surname, 'Colombo', 'Response body.surname should be "Colombo"');
+          done();
+        });
+    });
+    // #4
+    test('Send {surname: "da Verrazzano"}', function (done) {
+      chai
+        .request(server)
+        .put('/travellers')
+        .send({
+          "surname": "da Verrazzano"
+        })
+        .end((err, res) => {
+          assert.equal(res.status, 200, 'Response status should be 200');
+          assert.equal(res.type, 'application/json', 'Response type should be "application/json"');
+          assert.equal(res.body.name, 'Giovanni', 'Response body.name should be "Giovanni"');
+          assert.equal(res.body.surname, 'da Verrazzano', 'Response body.surname should be "da Verrazzano"');
+          done();
+        })
+    });
+  });
+});
+
+const Browser = require('zombie');
+
+Browser.site = 'https://boilerplate-mochachai.hypham.repl.co/';
+suite('Functional Tests with Zombie.js', function () {
+  const browser = new Browser();
+  this.timeout(5000);
+
+  suiteSetup(function(done) {
+    return browser.visit('/', done);
+  });
+
+  suite('Headless browser', function () {
+    test('should have a working "site" property', function() {
+      assert.isNotNull(browser.site);
+    });
+  });
+
+  suite('"Famous Italian Explorers" form', function () {
+    // #5
+    test('Submit the surname "Colombo" in the HTML form', function (done) {
+      browser.fill('surname', 'Colombo').then(() => {
+        browser.pressButton('submit', () => {
+          browser.assert.success();
+          browser.assert.text('span#name', 'Cristoforo');
+          browser.assert.text('span#surname', 'Colombo');
+          browser.assert.elements('span#dates', 1);
+          done();
+        });
+      });
+    });
+    // #6
+    test('Submit the surname "Vespucci" in the HTML form', function (done) {
+      browser.fill('surname', 'Vespucci').then(() => {
+        browser.pressButton('submit', () => {
+          browser.assert.success();
+          browser.assert.text('span#name', 'Amerigo');
+          browser.assert.text('span#surname', 'Vespucci');
+          browser.assert.elements('span#dates', 1);
+          done();
+        });
+      });
+    });
+  });
+});
